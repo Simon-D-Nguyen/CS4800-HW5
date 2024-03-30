14b53cf41332f607f688b605b95d54244f54102e
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-29 21:42:30 -0700

    Added generic FoodItem to add future items

---
+++ src/main/java/NewFood.java
@@ -0,0 +1,10 @@
+public class NewFood extends FoodItem {
+
+    public NewFood(String foodName, double price){
+        super();
+        setFoodName(foodName);
+        setItemPrice(price);
+    }
+}
+
+
