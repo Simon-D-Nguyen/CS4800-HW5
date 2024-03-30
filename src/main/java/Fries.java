4ed1ed8399a39313a86f413bc91b12c7c14a5eea
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-29 23:16:49 -0700

    Added additional food items

---
+++ src/main/java/Fries.java
@@ -0,0 +1,8 @@
+public class Fries extends FoodItem{
+
+    public Fries(){
+        super();
+        setFoodName("Fries");
+        setItemPrice(1.00);
+    }
+}
---
+++ src/main/java/HotDog.java
@@ -0,0 +1,8 @@
+public class HotDog extends FoodItem {
+
+    public HotDog(){
+        super();
+        setFoodName("Hot Dog");
+        setItemPrice(1.50);
+    }
+}
