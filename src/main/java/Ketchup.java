dbb7856ac05dd291659741dc0cfad2c15110b18d
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-29 21:46:28 -0700

    Added Ketchup as a topping for Decorator

---
+++ src/main/java/Ketchup.java
@@ -0,0 +1,7 @@
+public class Ketchup extends Topping {
+
+    public Ketchup(FoodItem foodItem){
+        super(foodItem);
+        addTopping("Ketchup", 0.5);
+    }
+}
---
+++ src/main/java/Onions.java
@@ -0,0 +1,7 @@
+public class Onions extends Topping{
+
+    public Onions(FoodItem foodItem) {
+        super(foodItem);
+        addTopping("Onions", 1.0);
+    }
+}
